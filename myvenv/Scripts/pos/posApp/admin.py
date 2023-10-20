7ea3bb830a968590c0def4ae65ca5a11b0c298f6
amanat <aliyounas6216@gmail.com>
2023-10-19 20:04:11 -0700

    suplier add

---
+++ myvenv/Scripts/pos/posApp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+
+
+
+admin.site.register(Supplier)
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Sales)
+admin.site.register(salesItems)
+# admin.site.register(Employees)
